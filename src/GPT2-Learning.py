b3694ffe239b556a6e28677d206c0977f97f2b23
heshgggg <87483961+heshgggg@users.noreply.github.com>
2023-03-27 11:46:17 +0500

    Update GPT2-Learning.py

--- src/GPT2-Learning.py
+++ src/GPT2-Learning.py
@@ -540,7 +540,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("BOTKEY", use_context=False)
+    updater = Updater("5734929195:AAGndD2og7gzFFY4Ak9TpKmGHPXAmIZSzGo", use_context=False)
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
     # on different commands - answer in Telegram
